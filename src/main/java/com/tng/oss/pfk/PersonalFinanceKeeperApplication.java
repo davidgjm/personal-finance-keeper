d0080b1b7fa6d1bbacd89ce46ab1e523085fdab2
david <davidgjm@gmail.com>
2021-02-13 23:26:46 +0800

    Initial project setup

---
+++ src/main/java/com/tng/oss/pfk/PersonalFinanceKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.tng.oss.pfk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tng/oss/pfk/PersonalFinanceKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tng.oss.pfk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
